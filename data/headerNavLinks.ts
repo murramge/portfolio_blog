b1bede843f33469beec08f68d59a07d87d3c0296
LESANF <nagong1000@naver.com>
2025-02-03 04:34:21 +0900

    fix: master branch node_modules 재설치

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
+// 이력서 파일 경로 수정
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/projects', title: 'Portfolio' },
   { href: '/강은화_이력서.pdf', title: 'Resume', download: true },
 ]
-
 export default headerNavLinks
