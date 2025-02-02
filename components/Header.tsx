ec3f154bc521e6576a1f242fbd690ec69808e23b
LESANF <nagong1000@naver.com>
2025-02-03 04:16:50 +0900

    feat: resume 링크 pdf다운로드로 대체

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,15 +28,26 @@ const Header = () => {
       <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
         {headerNavLinks
           .filter((link) => link.href !== '/')
-          .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
-            >
-              {link.title}
-            </Link>
-          ))}
+          .map((link) =>
+            link.download ? (
+              <a
+                key={link.title}
+                href={link.href}
+                className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
+                download
+              >
+                {link.title}
+              </a>
+            ) : (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
+              >
+                {link.title}
+              </Link>
+            )
+          )}
         <SearchButton />
         <ThemeSwitch />
         <MobileNav />
